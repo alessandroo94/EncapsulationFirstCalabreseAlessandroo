156404cccaa0ef5b32e0be63b7e7b154a501d116
alessandroo94 <121314484+alessandroo94@users.noreply.github.com>
2023-01-18 10:43:20 +0100

    Update Main.java

--- src/it/develhope/Main.java
+++ src/it/develhope/Main.java
@@ -1,5 +1,17 @@
+package it.develhope;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        House house = new House();
+        house.setAddress("Via Roma");
+        house.setFloorsNumber(22);
+
+        String[] resident = {"Alessando", "Pippo"};
+        house.setResidentsNames(resident);
+
+        System.out.println(String.format("Address: " + house.getAddress()));
+        System.out.println(String.format("Floors Number: " + house.getFloorsNumber()));
+        System.out.println(String.format("Resident: " + Arrays.toString(house.getResidentsNames())));
     }
-}
