078b7683c0196b54e7fe7dd9fd9e8f26ad6494a1
alessandroo94 <121314484+alessandroo94@users.noreply.github.com>
2023-01-19 15:54:29 +0100

    Update Main.java

--- src/it/develhope/Main.java
+++ src/it/develhope/Main.java
@@ -1,18 +1,22 @@
 package it.develhope;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         House house = new House();
-        house.setAddress("Via Roma");
-        house.setFloorsNumber(22);
+        Scanner input = new Scanner(System.in);
 
-        String[] resident = {"Alessando", "Pippo"};
-        house.setResidentsNames(resident);
+        System.out.println("Enter the residents name: ");
+        house.setResidentsNames(input.nextLine().split(","));
 
-        System.out.println(String.format("Address: " + house.getAddress()));
-        System.out.println(String.format("Floors Number: " + house.getFloorsNumber()));
-        System.out.println(String.format("Resident: " + Arrays.toString(house.getResidentsNames())));
-    }
-}
+        System.out.println("Enter the floors number: ");
+        house.setFloorsNumber(input.nextInt());
+
+        System.out.println("Enter the address: ");
+        house.setAddress(input.nextLine());
+
+
+        System.out.println(String.format("Floors number: %d, Address: %s, Residents: %s",
+                house.getFloorsNumber(), house.getAddress(), String.join(",", house.getResidentsNames())));
